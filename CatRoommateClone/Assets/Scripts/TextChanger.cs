9290d91cef103480c2523be379af75abbe6b0f1a
Ronojoy Mitra <Ezioweasley123#>
2019-03-04 18:29:37 -0500

    Added code to make the buttons switch between the questions

---
+++ CatRoommateClone/Assets/Scripts/TextChanger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextChanger : MonoBehaviour
+{
+    public TextMeshProUGUI Question;
+    public string[] Questions;
+    public TextMeshProUGUI Answer1;
+    public string[] Answer1A;
+    public TextMeshProUGUI Answer2;
+    public string[] Answer2A;
+    int count = 0;
+
+    private void Start()
+    {
+        Question.text = Questions[0];
+        Answer1.text = Answer1A[0];
+        Answer2.text = Answer2A[0];
+    }
+    private void Update()
+    {
+        
+    }
+    public void OnLeftClick()
+    {
+      count++;
+        //for (; count <= 7;)
+        if(count < 7)
+        {
+            Question.text = Questions[count];
+            Answer1.text = Answer1A[count];
+            Answer2.text = Answer2A[count];
+        }
+        
+    }
+    public void OnRightClick()
+    {
+        count++;
+        if (count < 7)
+        {
+            Question.text = Questions[count];
+            Answer1.text = Answer1A[count];
+            Answer2.text = Answer2A[count];
+        }
+    }
+}
