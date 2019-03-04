f6d6590639cee3aa09f5d751f6c16d74bce0f11c
Ronojoy Mitra <Ezioweasley123#>
2019-03-04 16:40:57 -0500

    Added all the answers to their arrays

--- CatRoommateClone/Assets/TextChanger.cs
+++ CatRoommateClone/Assets/TextChanger.cs
@@ -7,11 +7,15 @@ public class TextChanger : MonoBehaviour
 {
     public TextMeshProUGUI Question;
     public string[] Questions;
+    public TextMeshProUGUI Answer1;
+    public string[] Answer1A;
+    public TextMeshProUGUI Answer2;
+    public string[] Answer2A;
 
     private void Start()
     {
         Question.text = Questions[0];
+        Answer1.text = Answer1A[0];
+        Answer2.text = Answer2A[0];
     }
-    
-    
 }
