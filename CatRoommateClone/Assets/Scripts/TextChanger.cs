fdf17a504098499fba81389392fbc5790af3fe61
Ronojoy Mitra <Ezioweasley123#>
2019-03-11 22:09:27 -0400

    Final CatRoommate Clone
    Added cat sprite changes into code

--- CatRoommateClone/Assets/Scripts/TextChanger.cs
+++ CatRoommateClone/Assets/Scripts/TextChanger.cs
@@ -15,6 +15,8 @@ public class TextChanger : MonoBehaviour
     public Image _CatSprite;
     //public Image[] _CatSpritess;
     public Sprite[] _CatSprites;
+    public GameObject Button1;
+    public GameObject Button2;
     int _count = 0;
     int _correctCount = 0;
 
@@ -48,11 +50,20 @@ public class TextChanger : MonoBehaviour
         {
             _Question.text = _Questions[7];
             _CatSprite.sprite = _CatSprites[1];
+            //GameObject.Find("Answer1").GetComponent<Image>().enabled = false;
+            Button1.GetComponent<Image>().enabled = false;
+            Button1.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
+            Button2.GetComponent<Image>().enabled = false;
+            Button2.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
         }
         if (_count == 7 && _correctCount <= 3)
         {
             _Question.text = _Questions[8];
             _CatSprite.sprite = _CatSprites[7];
+            Button1.GetComponent<Image>().enabled = false;
+            Button1.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
+            Button2.GetComponent<Image>().enabled = false;
+            Button2.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
         }
     }
     public void OnLeftClick()
@@ -72,6 +83,31 @@ public class TextChanger : MonoBehaviour
             Debug.Log("Left answer increment");
             _correctCount++;
         }
+        if (_count == 1)
+        {
+            _CatSprite.sprite = _CatSprites[3];
+        }
+        if (_count == 2)
+        {
+            _CatSprite.sprite = _CatSprites[2];
+        }
+        if (_count == 3)
+        {
+            _CatSprite.sprite = _CatSprites[8];
+        }
+        if (_count == 4)
+        { 
+            _CatSprite.sprite = _CatSprites[9];
+        }
+        if (_count == 5)
+        {
+            _CatSprite.sprite = _CatSprites[4];
+        }
+        if (_count == 6)
+        {
+            _CatSprite.sprite = _CatSprites[1];
+        }
+      
         //return true;
     }
     public void OnRightClick()
@@ -88,6 +124,31 @@ public class TextChanger : MonoBehaviour
             Debug.Log("Right answer increment");
             _correctCount++;
         }
+        if (_count == 1)
+        {
+            _CatSprite.sprite = _CatSprites[11];
+        }
+        if (_count == 2)
+        {
+            _CatSprite.sprite = _CatSprites[0];
+        }
+        if (_count == 3)
+        {
+            _CatSprite.sprite = _CatSprites[2];
+        }
+        if (_count == 4)
+        {
+            _CatSprite.sprite = _CatSprites[5];
+        }
+        if (_count == 5)
+        {
+            _CatSprite.sprite = _CatSprites[2];
+        }
+        if (_count == 6)
+        {
+            _CatSprite.sprite = _CatSprites[10];
+        }
+     
         //return true;
     }
 }
